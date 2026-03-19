9f1e652ee17796947b91d9daddbe0731a5c667d6
pareesa969 <pareesakhan613@gmail.com>
2026-03-19 21:18:59 +0500

    refactor: remove python backend and migrate to pure frontend architecture

--- utils/randomizer.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def select_random_scenarios(scenarios, count=4):
-    """
-    Randomly shuffle scenarios and return selected ones.
-    """
-    random.shuffle(scenarios)
-    return scenarios[:count]
